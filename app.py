9bf38194d6c4257bb00ba656831724d32e8fddf9
Phoebe Kiluyi <134553522+Phoebe-Kiluyi@users.noreply.github.com>
2023-08-03 11:13:03 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+character_name = "Ben"
+character_age = "90"
+is_male = True
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + "years old. ")
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
+print()
+phrase = "Giraffe Academy"
+print(phrase)
+print(phrase + "is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[1])
+print(phrase.index("G"))
+print(phrase.replace("Giraffe","Bear"))
+print(3 * (4 + 5))
+print(10 % 3)
+my_num = 5
+print(str(my_num) + " my favorite number")
+from math import *
+my_num = -5
+print(abs(my_num))
+print(pow(4, 2))
+print(max(4, 2))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(25))
+name = input("Enter your name:")
+print("Hello" + name + "!")
+age = input("Enter your age:")
+print("Hello" + name + "! You are" + age)
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = int(num1) + int(num2)
+print(result)
